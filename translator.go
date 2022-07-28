3c7bd7a6e99960e699e6ffdabb27f24d8a97083a
root <root@vm-e36ee522.na4u.ru>
2022-07-28 20:11:53 +0000

    fixed translator lang detector

--- translator.go
+++ translator.go
@@ -3,7 +3,7 @@ package main
 import gt "github.com/bas24/googletranslatefree"
 
 func Translate(text, lang string) (string, error) {
-	result, err := gt.Translate(text, "from", lang)
+	result, err := gt.Translate(text, "auto", lang)
 
 	if err != nil {
 		return "", err
